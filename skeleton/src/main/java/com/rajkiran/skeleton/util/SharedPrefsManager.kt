3c90d1d737c95a77ce6dc2bf9750917a4f465659
Bande Rajkiran <bande.rajkiran@gmail.com>
2019-11-28 12:42:26 +0530

    Integrate Android KTX

--- skeleton/src/main/java/com/rajkiran/skeleton/util/SharedPrefsManager.kt
+++ skeleton/src/main/java/com/rajkiran/skeleton/util/SharedPrefsManager.kt
@@ -1,57 +1,41 @@
 package com.rajkiran.skeleton.util
 
-
 import android.content.Context
+import androidx.core.content.edit
 import androidx.preference.PreferenceManager
 
 object SharedPrefsManager {
 
     fun putInt(key: String, value: Int, context: Context) {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val editor = sharedPreferences.edit()
-        editor.putInt(key, value)
-        editor.apply()
+        PreferenceManager.getDefaultSharedPreferences(context).edit { putInt(key, value) }
     }
 
     fun getInt(key: String, defaultValue: Int, context: Context): Int {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return sharedPreferences.getInt(key, defaultValue)
+        return PreferenceManager.getDefaultSharedPreferences(context).getInt(key, defaultValue)
     }
 
     fun putLong(key: String, value: Long, context: Context) {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val editor = sharedPreferences.edit()
-        editor.putLong(key, value)
-        editor.apply()
+        PreferenceManager.getDefaultSharedPreferences(context).edit { putLong(key, value) }
     }
 
     fun getLong(key: String, defaultValue: Long, context: Context): Long {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return sharedPreferences.getLong(key, defaultValue)
+        return PreferenceManager.getDefaultSharedPreferences(context).getLong(key, defaultValue)
     }
 
     fun putBoolean(key: String, value: Boolean, context: Context) {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val editor = sharedPreferences.edit()
-        editor.putBoolean(key, value)
-        editor.apply()
+        PreferenceManager.getDefaultSharedPreferences(context).edit { putBoolean(key, value) }
     }
 
     fun getBoolean(key: String, defaultValue: Boolean, context: Context): Boolean {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return sharedPreferences.getBoolean(key, defaultValue)
+        return PreferenceManager.getDefaultSharedPreferences(context).getBoolean(key, defaultValue)
     }
 
     fun putString(key: String, value: String, context: Context) {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        val editor = sharedPreferences.edit()
-        editor.putString(key, value)
-        editor.apply()
+        PreferenceManager.getDefaultSharedPreferences(context).edit { putString(key, value) }
     }
 
     fun getString(key: String, defaultValue: String, context: Context): String? {
-        val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
-        return sharedPreferences.getString(key, defaultValue)
+        return PreferenceManager.getDefaultSharedPreferences(context).getString(key, defaultValue)
     }
 
 }
